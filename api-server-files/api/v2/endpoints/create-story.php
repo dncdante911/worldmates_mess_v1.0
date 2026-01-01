63da04a718e9166dbe9444e0981e27b33a7169cb
dncdante911 <worldmates.webmaster@gmail.com>
2026-01-01 11:32:26 +0200

    Update create-story.php

--- api-server-files/api/v2/endpoints/create-story.php
+++ api-server-files/api/v2/endpoints/create-story.php
@@ -1,13 +1,4 @@
 <?php
-// +------------------------------------------------------------------------+
-// | @author Deen Doughouz (DoughouzForest)
-// | @author_url 1: http://www.wowonder.com
-// | @author_url 2: http://codecanyon.net/user/doughouzforest
-// | @author_email: wowondersocial@gmail.com
-// +------------------------------------------------------------------------+
-// | WoWonder - The Ultimate Social Networking Platform
-// | Copyright (c) 2018 WoWonder. All rights reserved.
-// +------------------------------------------------------------------------+
 
 // Enable error logging for debugging
 error_reporting(E_ALL);
